793f58d329d4af15752c7a8e1ab8095a217c7387
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2020-09-30 10:27:07 -0300

    init

---
+++ tabela-refeicoes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabela-refeicoes
+//
+//  Created by Rodrigo Paixão on 30/09/20.
+//  Copyright © 2020 Rodrigo Paixão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
