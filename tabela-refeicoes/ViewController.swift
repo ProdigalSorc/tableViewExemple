f4f5ecb78d4176d4b4d44d6843d69ce75da599a3
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2020-10-02 13:50:22 -0300

    Configurando TableView

--- tabela-refeicoes/ViewController.swift
+++ tabela-refeicoes/ViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    let refeicoes = ["Churros", "Macarrao", "Pizza"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao
+        return celula
+    }
+    
 
 }
 
